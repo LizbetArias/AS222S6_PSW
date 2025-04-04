78332c9a4c34803742275e220b60210223441b40
Lizbet Arias Tinco <127221079+LizbetArias@users.noreply.github.com>
2025-04-04 23:23:56 +0000

    ERROR 4 Tests CORREGIDO

--- src/test/java/com/example/sonaranalysisdemo/SonarAnalysisDemoApplicationTests.java
+++ src/test/java/com/example/sonaranalysisdemo/SonarAnalysisDemoApplicationTests.java
@@ -6,8 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SonarAnalysisDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Este método está vacío porque se utiliza para verificar si el contexto de la aplicación se carga correctamente.
+        // Si no se necesita ninguna lógica adicional, podemos lanzar una excepción para indicar que la prueba no está implementada.
+        
+        throw new UnsupportedOperationException("Esta prueba aún no está implementada.");
+    }
 
 }
